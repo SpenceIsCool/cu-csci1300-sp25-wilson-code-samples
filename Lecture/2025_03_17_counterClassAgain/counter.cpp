2b38c4787ca0d116620053553c33fc4d9cd7300e
SpenceIsCool <spencer.wilson@colorado.edu>
2025-03-17 08:49:35 -0600

    added testing functions to counter.cpp

--- Lecture/2025_03_17_counterClassAgain/counter.cpp
+++ Lecture/2025_03_17_counterClassAgain/counter.cpp
@@ -19,6 +19,12 @@ using namespace std;
 
 // class declaration
 // at a quick glance we have a sense of what this class can do
+/**
+ * Counter
+ * TODO: later: a 2 digit counter can show numbers 0 - 99, be efficient
+ * TODO: later: get/set
+ * TODO: last: document the class expectations
+ */
 class Counter
 {
 public:
@@ -31,17 +37,17 @@ private:
 };  // don't forget the semi-colon
 
 
+// other function declarations
+void testDefault( );
+void testOverload( );
+
+
 // main
 // here we can make a class and call it's methods
 int main()
 {
-    Counter x{};
-    x.look();
-    x.click();
-    x.click();
-    x.look();
-    x.reset();
-    x.look();
+    testDefault( );
+    testOverload( );
 }
 
 
@@ -58,11 +64,12 @@ int main()
  * Counter::Counter
  * nameOfClass::nameOfMemberFunction
  * 
- * TODO: let us "overload" the constructor
+ * TODO: now: let us "overload" the constructor
  */
 Counter::Counter()
 {
     // Where did count come from?!? the function doesn't state it!
+    // ???
     count = 0;
 }
 
@@ -96,7 +103,7 @@ void Counter::look() const
  * return value: ???
  * 
  * TODO: play with ideas
- * - what if we round at 10 (small cliker) [change count type?]
+ * - what if we round at 100 (a two digit display)
  * - whta if we have the ability to do a many click?
  */
 void Counter::click()
@@ -119,5 +126,44 @@ void Counter::click()
 void Counter::reset()
 {
     count = 0;
+}
+
+
+// other function definitions
+void testDefault( )
+{
+    cout << endl << endl << endl
+         << "testDefault:" << endl
+         // \t is a tab character
+         << "\t1. I will create a counter object, and look at it once" << endl
+         << "\t2. click it twice, then look at it again" << endl
+         << "\t3. reset it, then look at it again" << endl
+         << endl;
+
+    cout << "BEGIN: testDefault: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl;
+    Counter x{};
+    x.look();
+    x.click();
+    x.click();
+    x.look();
+    x.reset();
+    x.look();
+    cout << "END: testDefault: <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+
+    return;
+}
+
+
+void testOverload( )
+{
+    cout << endl << endl << endl
+         << "TODO: test the overloading of the constructor" << endl
+         << endl;
+
+    cout << "testOverload: testDefault: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl;
+    // TODO
+    cout << "testOverload: testDefault: <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+
+    return;
 }
 
