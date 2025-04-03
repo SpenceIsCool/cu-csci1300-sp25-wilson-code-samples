cece815bdeaeb4b103101a5960530f31d314cc0c
SpenceIsCool <spencer.wilson@colorado.edu>
2025-04-02 18:27:35 -0600

    preparing an advanced solution

---
+++ Lecture/2025_04_04_detectives/Agency.h
@@ -0,0 +1,40 @@
+/**
+ * Agency.h
+ * 
+ * best practice from the book
+ * START: ifndef: if not defined
+ * END: endif: end this if block
+ * 
+ * OBSERVE: all of the code is in this block, so it will only happen once  
+ *          the condition is predicated on a variable named DETECTIVE_H
+ *          and the first line of the body of the condition defines DETECTIVE_H for us
+ */
+#ifndef AGENCY_H
+#define AGENCY_H
+
+
+#include "Detective.h"
+#include<string>
+
+class Agency
+{
+public:
+    Agency();
+    Agency( string name );
+    // TODO: Agency( staff )
+    void hire( Detective newStaff );
+    void solveNewCase( string detectiveName );
+    void solveNewCase( unsigned short index );
+    void showAllDetectives() const;
+    Detective getDetective( unsigned short index ) const;
+    Detective getDetective( string detectiveName ) const;
+    // TODO: setStaff
+private:
+    static const unsigned short _MAX_STAFF = 10;
+    string _name;
+    Detective _staff[_MAX_STAFF];
+    unsigned short _hired_so_far;
+};
+
+
+#endif
---
+++ Lecture/2025_04_04_detectives/Detective.cpp
@@ -0,0 +1,65 @@
+/** 
+ * Detective.cpp
+ * 
+ * often won't build this on it's own, but you can for faster builds later on...
+ * g++ -Wall -Werror -Wpedantic -Wsign-compare -std=c++17 -c Detective.cpp -o Detective.o
+ * 
+ * if you open Detective.o it will look like junk
+ * it's a file that only your computer can read
+ * and you can use it to build things like DetectiveDriver.cpp and AgencyDriver.cpp faster
+ * 
+ * defines all of the member functions that were declared in Detective.h
+ * be sure to prefix the function name with `Detective::`
+ * 
+ * best practice is to have a separate header file from the source file
+ * this allows us to change out implimentations if we need to 
+ * having a sigle header file
+ * and maybe many source sfiles based on different production needs
+ * Think about "modularity"
+ */
+#include "Detective.h"
+#include<iostream>
+using namespace std;
+
+
+Detective::Detective()
+{
+    _name = "no name known";
+    _solvedCases = 0;
+}
+
+Detective::Detective( string name, unsigned short solvedCases )
+{
+    _name = name;
+    _solvedCases = solvedCases;
+}
+
+void Detective::print() const
+{
+    cout << _name << " has solved " << _solvedCases << " cases." << endl;
+}
+
+string Detective::getName() const
+{
+    return _name;
+}
+
+
+unsigned short Detective::getSolvedCases() const
+{
+    return _solvedCases;
+}
+
+
+void Detective::setName( string name )
+{
+    _name = name;
+}
+
+
+void Detective::setSolvedCases( unsigned short solvedCases )
+{
+    _solvedCases = solvedCases;
+}
+
+
