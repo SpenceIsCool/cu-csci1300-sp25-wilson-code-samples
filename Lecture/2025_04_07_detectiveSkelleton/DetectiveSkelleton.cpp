25990d2d21c19bf700d2bc0247ca1e600a94e387
SpenceIsCool <spencer.wilson@colorado.edu>
2025-04-07 08:55:48 -0600

    added skelleton

---
+++ Lecture/2025_04_07_detectiveSkelleton/DetectiveSkelleton.cpp
@@ -0,0 +1,53 @@
+/** 
+ * DetectiveSkelleton.cpp
+ *
+ * return some default vaue for each function
+ * don't implement the functions
+ *
+ * you should be able to build the object file...
+ * g++ -std=c++17 -c DetectiveSkelleton.cpp -o DetectiveSkelleton.o
+ */
+#include "Detective.h"
+#include<iostream>
+using namespace std;
+
+
+Detective::Detective()
+{
+    return;
+}
+
+Detective::Detective( string name, unsigned short solvedCases )
+{
+    return;
+}
+
+void Detective::print() const
+{
+    return;
+}
+
+string Detective::getName() const
+{
+    return "";
+}
+
+
+unsigned short Detective::getSolvedCases() const
+{
+    return 0;
+}
+
+
+void Detective::setName( string name )
+{
+    return;
+}
+
+
+void Detective::setSolvedCases( unsigned short solvedCases )
+{
+    return;
+}
+
+
