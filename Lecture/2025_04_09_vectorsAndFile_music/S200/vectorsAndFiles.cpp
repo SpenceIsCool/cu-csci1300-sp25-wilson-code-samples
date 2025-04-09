e1bc00ee444f70f2f399a98a994a1611fb01bf3f
SpenceIsCool <spencer.wilson@colorado.edu>
2025-04-09 08:50:02 -0600

    prepared S200 stub for 4/9

---
+++ Lecture/2025_04_09_vectorsAndFile_music/S200/vectorsAndFiles.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+using namespace std;
+
+
+// struct Song
+// {
+//     string artist;
+//     string title;
+// };
+
+
+int main( )
+{
+    string fileName = "songs.txt";
+    return 0;
+}
+
+
