9d7297c69c4fdb128bf5374c5a7a42e4b0919b25
SpenceIsCool <spencer.wilson@colorado.edu>
2025-04-09 08:50:26 -0600

    added TODOs

--- Lecture/2025_04_07_detectiveSkelleton/DetectiveSkeleton.cpp
+++ Lecture/2025_04_07_detectiveSkelleton/DetectiveSkeleton.cpp
@@ -14,39 +14,51 @@ using namespace std;
 
 Detective::Detective()
 {
+    cout << "TODO" << endl;
     return;
 }
 
 Detective::Detective( string name, unsigned short solvedCases )
 {
+    cout << "TODO" << endl;   
     return;
 }
 
 void Detective::print() const
 {
+    cout << "TODO" << endl;
     return;
 }
 
 string Detective::getName() const
 {
+    cout << "TODO" << endl;
     return "";
 }
 
 
 unsigned short Detective::getSolvedCases() const
 {
+    cout << "TODO" << endl;
     return 0;
 }
 
 
+/**
+ * only set the name 
+ * if it is not already set to something
+ * don't let someone change the name
+ */
 void Detective::setName( string name )
 {
+    cout << "TODO" << endl;
     return;
 }
 
 
 void Detective::setSolvedCases( unsigned short solvedCases )
 {
+    cout << "TODO" << endl;
     return;
 }
 
